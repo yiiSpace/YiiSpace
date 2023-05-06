e99d7993e94fc33e026b668fe2e8474c47cf7daf
yiqing-95 <61391362@qq.com>
2023-05-06 16:10:26 +0800

    require 函数导致的变量名污染 使用即时函数可以解决

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    // FIXME 在运行consoleApplication时 这些咋传递过去的！！！
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
