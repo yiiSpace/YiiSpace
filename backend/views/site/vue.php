2d2f6ed217bfa6a856bf5020c3cedf81f5abbec0
yiqing-95 <61391362@qq.com>
2023-06-13 23:26:23 +0800

    测试下css功能

--- backend/views/site/vue.php
+++ backend/views/site/vue.php
@@ -2,6 +2,7 @@
 <?php
 
 use common\widgets\VueAsset;
+use year\widgets\CssBlock;
 use year\widgets\JsBlock;
 
 VueAsset::register($this);
@@ -10,7 +11,7 @@ VueAsset::register($this);
 
 <?=  __FILE__ ?>
 
-<div id="app">{{ message }}</div>
+<div id="app" class="vue-msg">{{ message }}</div>
 
 <?php JsBlock::begin() ?>
 <script>
@@ -25,3 +26,11 @@ VueAsset::register($this);
   }).mount('#app')
 </script>
 <?php JsBlock::end() ?>
+
+<?php CssBlock::begin() ?>
+<style>
+.vue-msg {
+  background-color: darkgoldenrod;
+}
+</style>
+<?php CssBlock::end() ?>
